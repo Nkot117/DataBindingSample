71a39b713ce143b8caf71f15bdeb03021c5c0bc8
Nkot117 <kumakura117@gmail.com>
2024-02-25 17:53:28 +0900

    不要なimportを削除

--- app/src/main/java/com/example/sampledatabinding/ItemAdapter.kt
+++ app/src/main/java/com/example/sampledatabinding/ItemAdapter.kt
@@ -1,11 +1,7 @@
 package com.example.sampledatabinding
 
-import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
--- app/src/main/java/com/example/sampledatabinding/MainActivity.kt
+++ app/src/main/java/com/example/sampledatabinding/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.sampledatabinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.RecyclerView
 import com.example.sampledatabinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,12 +10,13 @@ class MainActivity : AppCompatActivity() {
     private val itemAdapter by lazy { ItemAdapter() }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
         binding.taskList.adapter = itemAdapter
 
-        viewModel.tasks.observe(this,) {
+        viewModel.tasks.observe(this) {
             itemAdapter.submitList(it)
         }
     }
